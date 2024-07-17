9ba43abce8bdea39859bd41d896d9168b4d75ce5
Pelayo <pelayo.vieites.perez@udc.es>
2024-07-17 22:26:37 +0200

    #4 Reorganized data.service attributes

--- eeg-eye-state-simulator/src/app/data.service.ts
+++ eeg-eye-state-simulator/src/app/data.service.ts
@@ -5,39 +5,38 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private selectedSubjectSource = new BehaviorSubject<string>('');
-  private selectedWindowSource = new BehaviorSubject<number>(10);
-  private selectedTrainSizeSource = new BehaviorSubject<number>(70);
+  // BehaviorSubjects
   private graphDataSource = new BehaviorSubject<[number, number, number]>([-1, -1, -1]);
+  private selectedSubjectSource = new BehaviorSubject<string>('');
+  private selectedTrainSizeSource = new BehaviorSubject<number>(70);
+  private selectedWindowSource = new BehaviorSubject<number>(10);
   private simulationGraphStructSource = new BehaviorSubject<[number[], number[], string[]]>([[], [], []]);
   private telegramBotTokenSource = new BehaviorSubject<string>('');
   private telegramChatIdSource = new BehaviorSubject<string>('');
-  // private realGraphDataSource = new BehaviorSubject<number[]>([]);
-  // private predictedGraphDataSource = new BehaviorSubject<number[]>([]);
-  // private graphLabelsSource = new BehaviorSubject<string[]>([]);
 
-
-  selectedSubject$ = this.selectedSubjectSource.asObservable();
-  selectedWindow$ = this.selectedWindowSource.asObservable();
-  selectedTrainSize$ = this.selectedTrainSizeSource.asObservable();
+  // Observables for BehaviorSubjects
   graphData$ = this.graphDataSource.asObservable();
+  selectedSubject$ = this.selectedSubjectSource.asObservable();
+  selectedTrainSize$ = this.selectedTrainSizeSource.asObservable();
+  selectedWindow$ = this.selectedWindowSource.asObservable();
   simulationGraphStruct$ = this.simulationGraphStructSource.asObservable();
   telegramBotToken$ = this.telegramBotTokenSource.asObservable();
   telegramChatId$ = this.telegramChatIdSource.asObservable();
 
   constructor() { }
 
+  // Event triggers
+  updateGraphData(data: [number, number, number]) {
+    this.graphDataSource.next(data);
+  }
   updateSelectedSubject(subject: string) {
     this.selectedSubjectSource.next(subject);
-  }
-  updateSelectedWindow(window: number) {
-    this.selectedWindowSource.next(window);
   }
   updateTrainSize(trainSize: number) {
     this.selectedTrainSizeSource.next(trainSize);
   }
-  updateGraphData(data: [number, number, number]) {
-    this.graphDataSource.next(data);
+  updateSelectedWindow(window: number) {
+    this.selectedWindowSource.next(window);
   }
   updateSimulationGraphData(realData: number[], predictedDate: number[], labels: string[]) {
     this.simulationGraphStructSource.next([realData, predictedDate, labels]);
